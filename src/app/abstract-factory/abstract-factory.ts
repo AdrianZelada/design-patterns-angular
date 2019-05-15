e03b06f024d05c810c6bd37c5347231ff43f6608
adrian zelada torrez <zelada_torrez@hotmail.com>
2019-05-15 01:43:00 -0400

    form factory

--- src/app/abstract-factory/abstract-factory.ts
+++ src/app/abstract-factory/abstract-factory.ts
@@ -2,10 +2,11 @@ import {InjectionToken} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {UserService} from './form-user/user-service';
 import {StoreService} from './form-store/store-service';
+import {FormService} from './form.service';
 export const USER_SERVICE = new InjectionToken<any>('USER SERVICE');
 export const STORE_SERVICE = new InjectionToken<string>('STORE_SERVICE');
 
-export function AbstractFactoryMain(ServiceName, http: HttpClient) {
+export function AbstractFactoryMain(ServiceName, http: HttpClient): FormService {
   switch (ServiceName) {
     case 'USER_SERVICE':
       return new UserService(http);
